acfc72cab93f6af3f75ad05bc0dbbc00f9d3bec6
zhyuzh <zhyuzh@zhyuzhs-MacBook-Air-6.local>
2018-05-05 23:32:10 +0800

    增加图标管理MyIcons

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -3,7 +3,9 @@ import {
 } from 'react'
 import h from 'react-hyperscript'
 
-import Button from 'material-ui/Button';
+import Button from 'material-ui/Button'
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm'
+import FontAwesome from 'react-fontawesome'
 
 class Page extends Component {
     constructor(props) {
@@ -16,12 +18,22 @@ class Page extends Component {
     render() {
         return h('div', [
             h('h1', this.state.title),
+            h(AccessAlarmIcon, {
+                color: 'primary'
+            }),
+            h(FontAwesome, {
+                name: 'rocket',
+                color: 'red'
+            }),
+            h('img', {
+                src: 'icons/11.png'
+            }),
             h(Button, {
                 color: 'primary',
                 variant: 'raised',
                 onClick: () => {
                     global.XRouter.changePage('WelcomePage', {
-                        randNumber:Math.random(),
+                        randNumber: Math.random(),
                     })
                 },
             }, 'go welcome')
--- src/Pages/WelcomePage/WelcomePage.js
+++ src/Pages/WelcomePage/WelcomePage.js
@@ -3,25 +3,34 @@ import {
 } from 'react'
 import h from 'react-hyperscript'
 
-import Button from 'material-ui/Button';
+import Button from 'material-ui/Button'
+
+import MyIcons from '../../Utilities/MyIcons'
 
 class Page extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            title: 'WelcomePage',
+            title: 'WelcomePage!',
         }
     }
 
     render() {
         return h('div', [
             h('h1', this.state.title),
+            MyIcons.icon('favorite', {
+                color: 'primary'
+            }),
+            MyIcons.icon('favorite border', {
+                color: 'primary'
+            }),
+            h('div'),
             h(Button, {
                 color: 'primary',
                 variant: 'raised',
                 onClick: () => {
                     global.XRouter.changePage('HomePage', {
-                        randNumber:Math.random(),
+                        randNumber: Math.random(),
                     })
                 },
             }, 'go home')
